98d4a3969987721e7bd997734099920b7f7119fb
Mikhail Shkliaev <mikhail.shkliaev@live.ru>
2019-05-17 02:13:50 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stepsLabel = new System.Windows.Forms.Label();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topLabel
+            // 
+            this.topLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.topLabel.Location = new System.Drawing.Point(13, 48);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(240, 13);
+            this.topLabel.TabIndex = 0;
+            this.topLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(33, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(73, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество шагов:";
+            // 
+            // stepsLabel
+            // 
+            this.stepsLabel.AutoSize = true;
+            this.stepsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stepsLabel.Location = new System.Drawing.Point(139, 134);
+            this.stepsLabel.Name = "stepsLabel";
+            this.stepsLabel.Size = new System.Drawing.Size(0, 24);
+            this.stepsLabel.TabIndex = 3;
+            this.stepsLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(97, 170);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(75, 35);
+            this.resetBtn.TabIndex = 4;
+            this.resetBtn.Text = "Начать заново";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Visible = false;
+            this.resetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Угадайте число от 0 до 100";
+            // 
+            // checkBtn
+            // 
+            this.checkBtn.Location = new System.Drawing.Point(159, 75);
+            this.checkBtn.Name = "checkBtn";
+            this.checkBtn.Size = new System.Drawing.Size(75, 20);
+            this.checkBtn.TabIndex = 8;
+            this.checkBtn.Text = "Проверить";
+            this.checkBtn.UseVisualStyleBackColor = true;
+            this.checkBtn.Click += new System.EventHandler(this.CheckBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 217);
+            this.Controls.Add(this.checkBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.stepsLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.topLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Guess number";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label stepsLabel;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button checkBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        readonly string helloText = "Введите число в поле ниже";
+        readonly string smaller = "Не угадали - меньше. Повторите ввод";
+        readonly string bigger = "Не угадали - больше. Повторите ввод";
+        readonly string winText = "Поздравляю! Вы угадали число!";
+        readonly int from = 0, to = 100;
+        int i = 0, x, y;
+        readonly Random rnd = new Random();
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            stepsLabel.Text = i.ToString();
+            topLabel.Text = helloText;
+            textBox1.Text = "";
+            x = rnd.Next(from, to);
+            resetBtn.Visible = false;
+        }
+
+        private void CheckBtn_Click(object sender, EventArgs e)
+        {
+            y = Convert.ToInt32(textBox1.Text);
+            if (y < x)
+            {
+                i++;
+                if (i < 6)
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.DarkGreen;
+                }
+                else if (i >= 6 && i < 8)
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.Gold;
+                }
+                else
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.IndianRed;
+                }
+                topLabel.Text = bigger;
+                textBox1.Text = "";
+                stepsLabel.Text = i.ToString();
+            }
+            else if (y > x)
+            {
+                i++;
+                if (i < 6)
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.DarkGreen;
+                }
+                else if (i >= 6 && i < 8)
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.Gold;
+                }
+                else
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.IndianRed;
+                }
+                topLabel.Text = smaller;
+                textBox1.Text = "";
+                stepsLabel.Text = i.ToString();
+            }
+            else if (y == x)
+            {
+                if (i < 6)
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.DarkGreen;
+                }
+                else if (i >= 6 && i < 8)
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.Gold;
+                }
+                else
+                {
+                    stepsLabel.ForeColor = System.Drawing.Color.IndianRed;
+                }
+                topLabel.Text = winText;
+                resetBtn.Visible = true;
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            topLabel.Text = helloText;
+            x = rnd.Next(from, to);
+            stepsLabel.Text = i.ToString();
+        }
+    }
+}
